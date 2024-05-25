2cc9aaa04a61bd3e191046cfc4f743147f409086
hosain-mohamed <shs442000@gmail.com>
2024-05-25 19:45:16 +0300

    Make the field required

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,10 +5,15 @@ const SearchBar = () => {
   const { allItems, dispatch } = useContext(TodoListContext);
 
   const addItem = (e) => {
+    // check if input is not empty
+
     if (e.key === "Enter") {
+      if (e.target.value.trim() === "") {
+        e.target.setCustomValidity("Please add a Todo");
+        return;
+      }
       e.preventDefault();
       const id = allItems.length + 1;
-
       dispatch({
         type: "ADD",
         item: {
@@ -22,12 +27,14 @@ const SearchBar = () => {
   };
   return (
     <div className="input-container position-relative w-100 ">
-      <input
-        className="todo-input w-100 position-relative"
-        type="text"
-        placeholder="Add Todo"
-        onKeyDown={(e) => addItem(e)}
-      ></input>
+      <form onKeyDown={(e) => addItem(e)}>
+        <input
+          type="text"
+          className="todo-input w-100 position-relative"
+          placeholder="Add Todo"
+          required
+        />
+      </form>
     </div>
   );
 };
